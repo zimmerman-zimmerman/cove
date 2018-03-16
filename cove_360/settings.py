ba3871c2fe0bed9e1e227ae909011a1b77eeb98c
Ben Webb <ben.webb@opendataservices.coop>
2018-03-16 15:55:24 +0000

    Switch 360Giving schema to 186-duration-number branch
    https://github.com/ThreeSixtyGiving/standard/pull/238

--- cove_360/settings.py
+++ cove_360/settings.py
@@ -37,7 +37,7 @@ COVE_CONFIG = {
     'app_strapline': 'Convert, Validate, Explore 360Giving Data',
     'schema_name': '360-giving-package-schema.json',
     'schema_item_name': '360-giving-schema.json',
-    'schema_host': 'https://raw.githubusercontent.com/ThreeSixtyGiving/standard/master/schema/',
+    'schema_host': 'https://raw.githubusercontent.com/ThreeSixtyGiving/standard/186-duration-number/schema/',
     'schema_version': None,
     'schema_version_choices': None,
     'root_list_path': 'grants',
